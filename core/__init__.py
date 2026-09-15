bfb4a107ec6482c0dc8ea366298a4c15bbe2d67e
Lebow5ki <77303858+smokerdl@users.noreply.github.com>
2026-09-15 21:18:03 +0300

    Load Microsoft Translator provider

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,3 @@
 # Install core-wide translation policies before core submodules are imported.
 from . import default_provider_cooldown  # noqa: F401
+from . import microsoft_translate_patch  # noqa: F401
